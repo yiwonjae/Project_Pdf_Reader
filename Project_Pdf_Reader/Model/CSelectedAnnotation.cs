80e642a89177351d3cd4f6d7e5822791dc94358c
yiwonjae <mellowlee@naver.com>
2023-08-14 23:48:46 +0900

    annotation을 list에 추가하고 click하면 해당 page로 이동하는 기능 개발 완료함

---
+++ Project_Pdf_Reader/Model/CSelectedAnnotation.cs
@@ -0,0 +1,51 @@
+﻿using GalaSoft.MvvmLight.Command;
+using Syncfusion.Windows.PdfViewer;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Project_Pdf_Reader.Model
+{
+
+    public delegate void CallBack_Event_Annotation(CSelectedAnnotation a, PdfViewerControl b);
+
+    public class CSelectedAnnotation
+    {
+        
+        
+        public String context { get; set; }
+
+        public int page { get; set; }
+
+        public Rectangle contextBase { get;set; }
+
+        
+        
+        
+        public CallBack_Event_Annotation myCallback { get; set; }
+
+
+        //https://stackoverflow.com/questions/5073065/is-there-a-way-to-pass-a-parameter-or-multiple-params-to-the-callmethodaction
+
+        public CSelectedAnnotation()
+        {
+            CommandMouseDown = new RelayCommand<PdfViewerControl>((x) => CommandMouseDownFunction(x));
+        }
+
+        public ICommand CommandMouseDown { get; set; }
+
+        private void CommandMouseDownFunction(PdfViewerControl arg)
+        {
+            if (myCallback != null)
+            {
+                myCallback(this, arg);
+            }
+        }
+
+
+    }
+}
--- Project_Pdf_Reader/ViewModel/MainFrameViewModel.cs
+++ Project_Pdf_Reader/ViewModel/MainFrameViewModel.cs
@@ -8,11 +8,62 @@ using Syncfusion.Pdf.Graphics;
 using Syncfusion.Pdf.Interactive;
 using System.Collections.Generic;
 using System.Drawing;
+using Project_Pdf_Reader.Model;
 
 namespace Project_Pdf_Reader.ViewModel
 {
+
+
     public class MainFrameViewModel:ViewModelBase
     {
+        #region 변수
+
+        private List<CSelectedAnnotation> _Annotations;
+
+        #endregion
+
+        #region RaisePropertyChanged
+
+
+        public List<CSelectedAnnotation> Annotations
+        {
+            get { return this._Annotations; }
+            set
+            {
+                if (value != this._Annotations)
+                    this._Annotations = value;
+                RaisePropertyChanged("Annotations");
+            }
+        }
+
+        #endregion
+
+        public MainFrameViewModel()
+        {
+        
+        
+        }
+
+
+
+        #region Event
+
+
+        public void Event_Annotation(CSelectedAnnotation arg, PdfViewerControl control)
+        {
+            // scroll 하고 page를 이동해야 정상적으로 동작함
+            // page > scroll은 안됨 -- 확인함
+
+
+            //https://blazor.syncfusion.com/documentation/pdfviewer/how-to/move-scrollbar
+
+            control.ScrollTo(arg.contextBase.Location.X, arg.contextBase.Location.Y);
+
+
+            //https://help.syncfusion.com/wpf/pdf-viewer/navigating-through-the-pages
+            control.GotoPage(arg.page);
+
+        }
 
 
         public void PdfViewerControl_TextSelectionCompleted(object sender, Syncfusion.Windows.PdfViewer.TextSelectionCompletedEventArgs args)
@@ -21,7 +72,7 @@ namespace Project_Pdf_Reader.ViewModel
 
             int page = keys[0];
             String text = args.SelectedText;
-            var position    = args.SelectedTextInformation[page][text];
+            var position = args.SelectedTextInformation[page][text];
 
             String head = String.Format("X:{0},Y:{1},W:{2},H:{3}", position.X, position.Y, position.Width, position.Height);
 
@@ -36,13 +87,43 @@ namespace Project_Pdf_Reader.ViewModel
             textmarkup.TextMarkupAnnotationType = PdfTextMarkupAnnotationType.Highlight;
 
             textmarkup.Text = "Highlight Text";
+
+
             //Sets the highlighting color
             textmarkup.TextMarkupColor = new PdfColor(Color.LightPink);
 
 
-            ((PdfViewerControl)sender).LoadedDocument.Pages[page-1].Annotations.Add(textmarkup);
+            ((PdfViewerControl)sender).LoadedDocument.Pages[page - 1].Annotations.Add(textmarkup);
+
+            List<CSelectedAnnotation> temp = null;
+
+            if (Annotations != null)
+            {
+                temp = new List<CSelectedAnnotation>(Annotations);
+                Annotations.Clear();
+                Annotations = null;
+            }
+            else
+            {
+                temp = new List<CSelectedAnnotation>();
+            }
+
+
+
+            temp.Add(new CSelectedAnnotation { context = text, page = page, contextBase = position, myCallback= Event_Annotation });
+
+
+            Annotations = temp;
+
+
+
+
 
         }
+
+
+        #endregion
+
 
 
     }
